4bb5545d8188200e6f9ec39c23c5b711db5cb20b
Андрей <Andrei.Iagunov@urfu.me>
2023-10-04 17:55:11 +0500

    исправление ошибок линтера

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,17 @@
 function checkLength(str, maxLength){                          //1 задача
-  let length = str.length;
+  const length = str.length;
   if(length<=maxLength){
     return true;
   }
   return false;
 }
-console.log(checkLength('проверяемая строка', 20));
-console.log(checkLength('проверяемая строка', 18));
-console.log(checkLength('проверяемая строка', 10));
+
+checkLength('проверяемая строка', 20);
+checkLength('проверяемая строка', 18);
+checkLength('проверяемая строка', 10);
 
 function checkPalindrome(str) {                                //2 задача
-  let lowerCaseStr = str.toLowerCase().replaceAll(' ', '');
+  const lowerCaseStr = str.toLowerCase().replaceAll(' ', '');
   const length = lowerCaseStr.length;
   for (let i = 0; i < length / 2; i++) {
     if (lowerCaseStr.charAt(i)!== lowerCaseStr.charAt(length - 1 - i)) {
@@ -20,9 +21,9 @@ function checkPalindrome(str) {                                //2 задача
   return true;
 }
 
-console.log(checkPalindrome('топот'));
-console.log(checkPalindrome('ДовОд'));
-console.log(checkPalindrome('Кекс'));
+checkPalindrome('топот');
+checkPalindrome('ДовОд');
+checkPalindrome('Кекс');
 
 function checkNumber(string){                                  // 3 задача
   string = string.toString().replaceAll(' ', '');
@@ -35,6 +36,6 @@ function checkNumber(string){                                  // 3 задача
   return result===''? NaN: result;
 }
 
-console.log(checkNumber('2023 год'));
-console.log(checkNumber(('а я томат')));
+checkNumber('2023 год');
+checkNumber(('а я томат'));
 
